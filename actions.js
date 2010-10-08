9535c49edb53e025e6181cf824668eb0c3963fcc
Hannes Wallnöfer <hannesw@gmail.com>
2010-10-08 13:05:02 +0200

    Reformat code

--- actions.js
+++ actions.js
@@ -9,25 +9,20 @@ var log = require('ringo/logging').getLogger(module.id);
 var root = fs.absolute(require("./config").root);
 var welcomePages = ["index.html","index.md"];
 
-exports.index = function (req,path)
-{
+exports.index = function (req, path) {
     var uriPath = files.resolveUri(req.rootPath, path);
-    var absolutePath=fs.join(root,path);
+    var absolutePath = fs.join(root, path);
 
     checkRequest(uriPath);
 
-    if(fs.isFile(absolutePath))
-    {
+    if (fs.isFile(absolutePath)) {
         return serveFile(absolutePath);
     }
 
-    if(fs.isDirectory(absolutePath))
-    {
-        for each(var name in welcomePages)
-        {
-            if(fs.isFile(fs.join(absolutePath,name)))
-            {
-                return serveFile(fs.join(absolutePath,name));
+    if (fs.isDirectory(absolutePath)) {
+        for each(var name in welcomePages) {
+            if (fs.isFile(fs.join(absolutePath, name))) {
+                return serveFile(fs.join(absolutePath, name));
             }
         }
         if (!strings.endsWith(uriPath, "/")) {
@@ -38,14 +33,11 @@ exports.index = function (req,path)
     throw {notfound:true};
 };
 
-function listFiles(absolutePath,uriPath)
-{
-    var paths = fs.list(absolutePath).filter(function(file)
-    {
+function listFiles(absolutePath, uriPath) {
+    var paths = fs.list(absolutePath).filter(function(file) {
         return !files.isHidden(file);
-    }).sort().map(function(file)
-    {
-        var filePath = fs.join(absolutePath,file);
+    }).sort().map(function(file) {
+        var filePath = fs.join(absolutePath, file);
         var size;
         if (fs.isDirectory(filePath)) {
             size = fs.list(filePath).length + " files";
@@ -56,11 +48,11 @@ function listFiles(absolutePath,uriPath)
             name:file,
             size: size,
             lastModified: fs.lastModified(filePath),
-            path: files.resolveUri(uriPath,file)
+            path: files.resolveUri(uriPath, file)
         };
     });
 
-    var parentDir = uriPath == "/" ? "":"/../";
+    var parentDir = uriPath == "/" ? "" : "/../";
 
     return Response.skin(module.resolve('skins/list.html'), {
         files: paths,
@@ -69,10 +61,8 @@ function listFiles(absolutePath,uriPath)
     });
 }
 
-function serveFile(absolutePath)
-{
-    if(fs.extension(absolutePath)==".md")
-    {
+function serveFile(absolutePath) {
+    if (fs.extension(absolutePath) == ".md") {
         var html = md.Markdown().process(fs.read(absolutePath));
         return Response.skin(module.resolve('skins/page.html'), {
             content: html,
@@ -81,14 +71,11 @@ function serveFile(absolutePath)
     return Response.static(absolutePath);
 }
 
-function checkRequest(request)
-{
+function checkRequest(request) {
     var path = request.split('/');
 
-    for(var i=0;i<path.length;i++)
-    {
-        if(path[i]!="" && files.isHidden(path[i]))
-        {
+    for (var i = 0; i < path.length; i++) {
+        if (path[i] != "" && files.isHidden(path[i])) {
             throw {notfound:true};
         }
     }
