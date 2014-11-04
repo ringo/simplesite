04b57dadee9f4ce8389e66d5d4511285cef175ce
Philipp <philipp.naderer@gmail.com>
2014-11-04 14:51:15 +0100

    Extracts page title form markdown file

--- actions.js
+++ actions.js
@@ -1,10 +1,12 @@
 var fs = require("fs");
 var config = require("./main").config;
+var strings = require("ringo/utils/strings");
 var response = require("ringo/jsgi/response");
 var log = require("ringo/logging").getLogger(module.id);
 
 var {Application} = require("stick");
 var {Environment} = require("reinhardt");
+var markdown = require("ringo-commonmark");
 
 var templates = new Environment({
     loader: config.templates || module.resolve("./templates")
@@ -24,8 +26,34 @@ app.get(function(request, path) {
         return response.bad();
     }
 
-    var template = templates.getTemplate("base.html");
+    var mdFile = fs.join(config.contentDirectory, path);
+    if (strings.endsWith(mdFile, "/")) {
+        mdFile += "index.md";
+    } else {
+        mdFile += ".md";
+    }
+    log.info("File to load: " + mdFile);
+
+    if (!fs.exists(mdFile)) {
+        log.info("file not found: ", path, " - ", mdFile)
+        var notFound = templates.getTemplate("notFound.html");
+        return response.html(notFound.render({
+            "path": path
+        }));
+    }
+
+    var htmlContent = markdown.process(fs.read(mdFile, "r"));
+
+    // Extract title
+    var pageTitle;
+    if (strings.startsWith(htmlContent, "<h1>")) {
+        pageTitle = htmlContent.substring(4, htmlContent.indexOf("</h1>"));
+    }
+
+    var template = templates.getTemplate("page.html");
     return response.html(template.render({
-        "path": path
+        "path": path,
+        "pageTitle": pageTitle,
+        "content": htmlContent
     }));
 });
--- main.js
+++ main.js
@@ -13,21 +13,23 @@ if (!configFile || !fs.exists(configFile)) {
 }
 
 if (!fs.exists(configFile)) {
-   console.err("Missing config file", configFile);
-   system.exit(1);
+    console.err("Missing config file", configFile);
+    system.exit(1);
 }
 
 var config;
 try {
-   config = exports.config = JSON.parse(fs.read(configFile));
-   if (!config.contentDirectory || !fs.exists(config.contentDirectory)) {
-       console.err("Invalid content directory in config: " + config.contentDirectory);
-       system.exit(1);
-   }
+    config = exports.config = JSON.parse(fs.read(configFile));
+    if (!config.contentDirectory || !fs.exists(config.contentDirectory)) {
+        console.err("Invalid content directory in config: " + config.contentDirectory);
+        system.exit(1);
+    }
 } catch (e) {
-   console.log("Error parsing config file '", configFile ,"': ", e);
-   system.exit(1);
+    console.log("Error parsing config file '", configFile ,"': ", e);
+    system.exit(1);
 }
+var loggingConfig = getResource((config.logging ? config.logging : module.resolve("./config/log4j.properties")));
+require("ringo/logging").setConfig(loggingConfig, true);
 
 // main script to start application
 if (require.main == module) {
