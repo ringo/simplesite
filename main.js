613ec5bd0ffe2ec9400f8ec8712a1ad252aba5a1
Philipp Naderer <philipp.naderer@gmail.com>
2015-04-29 16:37:41 +0200

    main.js is daemon-compatible now

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-//#!/usr/bin/env ringo
-var fs = require("fs");
-var system = require("system");
+var log = require("ringo/logging").getLogger(module.id);
+var engine = require("ringo/engine");
 var config = require("./config");
 
-var WebApp = exports.WebApp = module.resolve("./webapp");
+var {Server} = require("ringo/httpserver");
 
-if (require.main == module.id) {
-    var {Server} = require("ringo/httpserver");
-    var httpServer = new Server({
-        "appName": "app",
-        "appModule": WebApp,
-        "port": config.get("port") || 8080
-    });
+var httpServer = new Server({
+    "appName": "app",
+    "appModule": module.resolve("./webapp"),
+    "port": config.get("port") || 8080
+});
+
+/**
+ * Called when the application starts
+ */
+var start = exports.start = function() {
+    log.info("Starting simplesite application");
     httpServer.start();
+    // register shutdown hook to stop ftp server
+    engine.addShutdownHook(function() {
+        stop();
+    });
+};
+
+/**
+ * Called when the engine is shut down
+ */
+var stop = exports.stop = function() {
+    httpServer.stop();
+    httpServer.destroy();
+    store.connectionPool.close();
+    log.info("Stopped simplesite application");
+};
+
+//Script run from command line
+if (require.main === module) {
+    start();
 }
